2911735a4b721a7eb76e32f8139ff0907b0f4f09
julsy-chen <thejuliachen@gmail.com>
2026-07-02 16:57:37 -0700

    add tiebreaker and full column edge case + refactor connect 4 logic inot oop class

--- game.py
+++ game.py
@@ -1,113 +1,134 @@
-gameBoard = [["." for _ in range(7)] for _ in range(6)]
-gameBoard.append(list(range(7)))
+class Connect4:
+    def __init__(self):
+        # initialize state to get a new board
+        self.board = [["." for _ in range(7)] for _ in range(6)]
+        self.board.append(list(range(7)))
+        self.nextRowForColumn = dict.fromkeys(range(7), 5)
 
-nextRowForColumnMap = dict.fromkeys(range(7), 5)
+        self.turn = 0
+        self.disks = ["O", "X"]
+        self.gameActive = True
 
-print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in gameBoard]))
+    def printBoard(self):
+        # displays the current state of the board for the players to see
+        print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in self.board]))
 
-turn = 0
-disks = ["O", "X"]
+    def insertChip(self, column, playerDisk):
+        # find row for where the disk falls to
+        diskRow = self.nextRowForColumn[column]
+        # update nextRowForColumnMap
+        self.nextRowForColumn[column]-=1
+        # update self.board
+        self.board[diskRow][column] = playerDisk
+        # output the row that the playerDisk has fallen to
+        print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in self.board]))
+        return diskRow
 
-diskColumn = 0
-diskRow = 0
+    def getPlayerInput(self, playerNumber):
+        while True:
+            try:
+                # get player input
+                givenInput = int(input(f"Player {playerNumber}'s move: "))
 
-game=True
+                if 0 <= givenInput <= 6:
+                    if self.nextRowForColumn[givenInput] < 0:
+                        print("You dropped your disk onto a full column, it topples out pathetically. Try again")
+                    else:
+                        return givenInput # Valid input exits the function immediately
+                else:
+                    print(f"You dropped your disk outside of the gameboard, silly guy! Try again")
+                    
+            except ValueError:
+                print("You seem a bit confused, try again, choosing a number between 0 and 6.")
 
-# input: column that we want to insert, player putting disk
-# output: diskRow
-def insertChip(column, playerDisk):
-    # find row for where the disk falls to
-    diskRow = nextRowForColumnMap[column]
-    # update nextRowForColumnMap
-    nextRowForColumnMap[column]-=1
-    # update gameBoard
-    gameBoard[diskRow][column] = playerDisk
-    # output the row that the playerDisk has fallen to
-    return diskRow
+    def checkWin(self, diskRow, diskColumn, playerDisk):
+        # check all directions for a win
+        counterRow = 0
+        counterDown = 0
+        counterSlash = 0
+        counterBackslash = 0
 
-def playerInput(playerNumber):
-    while True:
-        try:
-            # get player input
-            givenInput = int(input(f"Player {playerNumber}'s move: "))
-
-            if 0 <= givenInput <= 6:
-                return givenInput # Valid input exits the function immediately
+        c = 1
+        while 0<=diskColumn - c: #check left side
+            if self.board[diskRow][diskColumn-c] == playerDisk:
+                counterRow += 1
+                c+=1
             else:
-                print(f"You dropped your disk outside of the gameboard, silly guy! Try again, choosing a number between 0 and 6.")
-                
-        except ValueError:
-            print("You seem a bit confused, try again, choosing a number between 0 and 6.")
+                break
+        c=1
+        while diskColumn + c <=6: #check right side
+            if self.board[diskRow][diskColumn+c] == playerDisk:
+                counterRow += 1
+                c+=1
+            else:
+                break
+        
+        c=1
+        while diskRow + c <=5: #checking down
+            if self.board[diskRow+c][diskColumn] == playerDisk:
+                counterDown +=1
+                c+=1
+            else:
+                break
 
-while game: #game loop
-    playerNumber = (turn%2)+1
-    playerDisk = disks[int(playerNumber)-1]
-    diskColumn = playerInput(playerNumber)
-    diskRow = insertChip(diskColumn, playerDisk) # --> updated the game board + set row and column that the disk has fallen into
-    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in gameBoard]))
+        c=1
+        while 0<=diskRow-c and diskColumn+c <=6: #checking NE
+            if self.board[diskRow-c][diskColumn+c] == playerDisk:
+                counterSlash +=1
+                c+=1
+            else:
+                break
 
-    c = 1
-    counterRow = 0
-    counterDown = 0
-    counterSlash = 0
-    counterBackslash = 0
+        c=1
+        while diskRow+c<=5 and 0<=diskColumn-c: #checking SW
+            if self.board[diskRow+c][diskColumn-c]== playerDisk:
+                counterSlash +=1
+                c+=1
+            else:
+                break
+        
+        c=1
+        while 0<=diskRow-c and 0<=diskColumn-c: #checking NW
+            if self.board[diskRow-c][diskColumn-c]== playerDisk:
+                counterBackslash +=1
+                c+=1
+            else:
+                break
 
-    while 0<=diskColumn - c: #check left side
-        if gameBoard[diskRow][diskColumn-c] == playerDisk:
-            counterRow += 1
-            c+=1
-        else:
-            break
-    c=1
-    while diskColumn + c <=6: #check right side
-        if gameBoard[diskRow][diskColumn+c] == playerDisk:
-            counterRow += 1
-            c+=1
-        else:
-            break
+        c=1
+        while diskRow+c<=5 and diskColumn+c<=6: #checking SE
+            if self.board[diskRow+c][diskColumn+c]== playerDisk:
+                counterBackslash +=1
+                c+=1
+            else:
+                break
+        return (counterRow >= 3 or counterDown >= 3 or counterSlash >= 3 or counterBackslash >= 3)
     
-    c=1
-    while diskRow + c <=5: #checking down
-        if gameBoard[diskRow+c][diskColumn] == playerDisk:
-            counterDown +=1
-            c+=1
-        else:
-            break
+    def play(self):
+        # game loop
+        self.printBoard()
 
-    c=1
-    while 0<=diskRow-c and diskColumn+c <=6: #checking NE
-        if gameBoard[diskRow-c][diskColumn+c] == playerDisk:
-            counterSlash +=1
-            c+=1
-        else:
-            break
+        while self.gameActive:
+            playerNumber = (self.turn%2)+1
+            playerDisk = self.disks[playerNumber-1]
 
-    c=1
-    while diskRow+c<=5 and 0<=diskColumn-c: #checking SW
-        if gameBoard[diskRow+c][diskColumn-c]== playerDisk:
-            counterSlash +=1
-            c+=1
-        else:
-            break
-    
-    c=1
-    while 0<=diskRow-c and 0<=diskColumn-c: #checking NW
-        if gameBoard[diskRow-c][diskColumn-c]== playerDisk:
-            counterBackslash +=1
-            c+=1
-        else:
-            break
+            diskColumn = self.getPlayerInput(playerNumber)
+            diskRow = self.insertChip(diskColumn, playerDisk)
 
-    c=1
-    while diskRow+c<=5 and diskColumn+c<=6: #checking SE
-        if gameBoard[diskRow+c][diskColumn+c]== playerDisk:
-            counterBackslash +=1
-            c+=1
-        else:
-            break
+            self.printBoard
 
-    if counterRow >= 3 or counterDown >= 3 or counterSlash >= 3 or counterBackslash >= 3:
-        print(f"Player {playerNumber} Wins!")
-        break
+            if self.checkWin(diskRow, diskColumn, playerDisk):
+                print(f"Player {playerNumber} Wins!")
+                self.gameActive = False
+                break
 
-    turn +=1
+            if all(val < 0 for val in self.nextRowForColumn.values()):
+                print("It's a draw!")
+                self.gameActive = False
+                break
+            
+            self.turn += 1
+
+if __name__ == "__main__":
+    game = Connect4()
+    game.play()
